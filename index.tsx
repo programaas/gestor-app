19ff0a8855ac32476e96f7d44369e37ebe462975
programaaasss <programaaasss@gmail.com>
2025-10-05 10:27:13 +0000

    fix(deploy): Remove import de CSS conflitante para usar apenas CDN

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Importa o CSS principal com os estilos do Tailwind
 import App from './App';
 
 const root = ReactDOM.createRoot(
